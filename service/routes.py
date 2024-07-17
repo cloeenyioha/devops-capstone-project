63bcda0bed89da7ff3a667d17614d5fd00acd0af
cloeenyioha <cloe001@yahoo.com>
2024-07-17 09:29:25 -0400

    added link task

--- service/routes.py
+++ service/routes.py
@@ -57,10 +57,13 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
 @app.route("/accounts", methods=["GET"])
+
+
 def list_accounts():
     """
     List all Accounts
@@ -78,6 +81,8 @@ def list_accounts():
 # READ AN ACCOUNT
 ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["GET"])
+
+
 def get_accounts(account_id):
     """
     Reads an Account
@@ -134,8 +139,6 @@ def delete_accounts(account_id):
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
-
 def check_content_type(media_type):
     """Checks that the media type is correct"""
     content_type = request.headers.get("Content-Type")
@@ -146,3 +149,4 @@ def check_content_type(media_type):
         status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
         f"Content-Type must be {media_type}",
     )
+
