79888f378cc39714134bcb692cd6c30c000bb65b
cloeenyioha <cloe001@yahoo.com>
2024-07-17 11:02:46 -0400

    fix again

--- service/routes.py
+++ service/routes.py
@@ -73,7 +73,7 @@ def list_accounts():
     account_list = [account.serialize() for account in accounts]
 
     app.logger.info("Returning [%s] accounts", len(account_list))
-    return jsonify(acount_list), status.HTTP_200_OK
+    return jsonify(account_list), status.HTTP_200_OK
 
 ######################################################################
 # READ AN ACCOUNT
